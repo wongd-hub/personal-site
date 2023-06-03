e17fa7a9ad077f10158c31643cd97a30cc55cc1c
Darren Wong <email@darrenwong.net>
2023-06-03 15:40:05 +1000

    Added rudimentary animations

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -5,18 +5,36 @@ export default function Landing() {
   return (
     <banner className="landing-container" id="landing" role="banner">
       <div className="title-container">
-        <h1
-          className="title-text gradient-text noselect"
-          role="heading"
-          aria-level="1"
-        >
-          Darren<br/>Wong
-        </h1>
-        <h2 className="gradient-text noselect">
-          data analyst<br/>
-          blogger<br/>
-          aspiring web dev
-        </h2>
+        <AnimatePresence>
+          <motion.h1
+            className="title-text gradient-text noselect"
+            role="heading"
+            aria-level="1"
+            initial={{ y: 100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{
+              duration: 3.5,
+              delay: 0.5
+            }}
+          >
+            {/* We'll want to stagger these two using staggerChildren at some point */}
+            Darren<br/>Wong
+          </motion.h1>
+          <motion.h2 
+            className="gradient-text noselect"             
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{
+              duration: 3,
+              delay: 1,
+              ease: [0, 0.71, 0.2, 1.01]
+            }}
+          >
+            data analyst<br/>
+            blogger<br/>
+            aspiring web dev
+          </motion.h2>
+        </AnimatePresence>
       </div>
       <div className="arrow-down noselect gradient-text"></div>
     </banner>
