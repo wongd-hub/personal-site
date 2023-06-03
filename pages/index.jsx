42697de3e0eb3a4e53d3f97a5589a5848d8258da
Darren Wong <email@darrenwong.net>
2023-06-04 00:34:50 +1000

    Configured favicon

--- pages/index.jsx
+++ pages/index.jsx
@@ -11,10 +11,10 @@ export default function Home() {
         <title>Darren Wong</title>
         <meta name="description" content="Darren Wong's portfolio" />
         <link rel="icon" href="/assets/favicon/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
       </Head>
 
       <RippleScene
