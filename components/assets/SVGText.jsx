458d01c7be2b5ce52f8512e6955c6d423e38686f
Darren Wong <email@darrenwong.net>
2023-06-04 22:05:54 +1000

    Added media query equivalent for size of landing links

--- components/assets/SVGText.jsx
+++ components/assets/SVGText.jsx
@@ -3,7 +3,6 @@ import SVGGradient from "./SVGGradient"
 import { useEffect, useState } from "react"
 
 const SVGText = ({ 
-    // x = null, y = null, 
     fontSize = 18, 
     children, 
     ...props 
@@ -16,32 +15,48 @@ const SVGText = ({
     const [Measurements, setMeasurements] = useState({remSize: 0, calculatedWidth: 0});
 
     useEffect(() => {
-        const fontSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
-        setMeasurements({
-            remSize: fontSize,
-            calculatedWidth: (
-                // Get length of the textbox based 
-                fontSize * 1.275 * children.length * 
-                // For this particular font, the ratio of width to height 
-                // on one character. Only really works here because this is 
-                // a monospace font. Measured by looking at the SVG bounding 
-                // box for one character
-                13.54 / (16 * 1.275) *
-                // This ratio isn't exact, so we'll scale it down based on 
-                // the observed ratio of text area width to SVG width.
-                // We can use this with confidence since we'll also assert this as
-                // the text box length and stretch text to fit it using textLength 
-                // and lengthAdjust
-                scale
-            )
-        });
+        const updateMeasurements = () => {
+            const remSize = parseFloat(getComputedStyle(document.documentElement).fontSize);
+            const multiplier = window.innerWidth < 500 ? 1 : 1.275; // Can turn into an if-else later
+
+            setMeasurements({
+                fontSize: remSize * multiplier,
+                calculatedWidth: (
+                    // Get length of the textbox based 
+                    remSize * multiplier * children.length * 
+                    // For this particular font, the ratio of width to height 
+                    // on one character. Only really works here because this is 
+                    // a monospace font. Measured by looking at the SVG bounding 
+                    // box for one character
+                    13.54 / (16 * 1.275) *
+                    // This ratio isn't exact, so we'll scale it down based on 
+                    // the observed ratio of text area width to SVG width.
+                    // We can use this with confidence since we'll also assert this as
+                    // the text box length and stretch text to fit it using textLength 
+                    // and lengthAdjust
+                    scale
+                ),
+            });
+        }
+
+        // Call once to set initial measurements
+        updateMeasurements();
+
+        // Listen to window resize event
+        window.addEventListener('resize', updateMeasurements);
+
+        // Clean up event listener on unmount
+        return () => {
+            window.removeEventListener('resize', updateMeasurements);
+        }
+
     }, []);
 
     return (
         <svg 
             xmlns="http://www.w3.org/2000/svg" 
             width={Measurements.calculatedWidth}
-            height={Measurements.remSize * 1.275} 
+            height={Measurements.fontSize} 
         >
             <defs>
                 <SVGGradient id={id}/>
@@ -58,7 +73,7 @@ const SVGText = ({
                 // have a relatively small effect since we've applied a scaling 
                 // factor to get a more accurate width
                 lengthAdjust="spacing"
-                fontSize={Measurements.remSize * 1.275}
+                fontSize={Measurements.fontSize}
                 textAnchor="middle" // To align text in center
                 dominantBaseline="middle" // To align text in middle vertically
                 {...props}
