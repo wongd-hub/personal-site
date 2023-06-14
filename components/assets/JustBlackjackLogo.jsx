4d5ac29aec0e092ffeba4eb91c7fa9fef07a8c24
Darren Wong <me@darrenwong.id.au>
2023-06-14 12:40:10 +1000

    Added note

--- components/assets/JustBlackjackLogo.jsx
+++ components/assets/JustBlackjackLogo.jsx
@@ -3,6 +3,8 @@ import Lottie from "lottie-react";
 import justBlackjackAnim from "./justBlackjackLottie.json";
 
 const JustBlackjackLogo = ({ ...props }) => {
+
+    // TODO: Allow passing of size props into this; maybe changing the style prop etc.
     return <Lottie animationData={justBlackjackAnim} {...props} />;
 };
 
