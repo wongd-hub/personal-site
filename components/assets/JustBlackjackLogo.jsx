cdf2c5f5ddd72502dfe99116af9120a7f7a3a7ea
Darren Wong <me@darrenwong.id.au>
2023-06-15 10:10:50 +1000

    Added more notes for revisit

--- components/assets/JustBlackjackLogo.jsx
+++ components/assets/JustBlackjackLogo.jsx
@@ -5,6 +5,8 @@ import justBlackjackAnim from "./justBlackjackLottie.json";
 const JustBlackjackLogo = ({ ...props }) => {
 
     // TODO: Allow passing of size props into this; maybe changing the style prop etc.
+    // Put all the reactivity in here instead of out in the main landing Component
+    // See if you can put the animation code here as well
     return <Lottie animationData={justBlackjackAnim} {...props} />;
 };
 
