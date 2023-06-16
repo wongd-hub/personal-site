b4f6a397748e4ab52224695acfa6166d25fed98f
Darren Wong <me@darrenwong.id.au>
2023-06-16 14:53:45 +1000

    Encapsulated JustBlackjackLogo anims into its own component

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -6,11 +6,13 @@ import Socials from "./socials/Socials";
 import { SocialHoverContext } from "./contexts/SocialHoverContext";
 import JustBlackjackLogo from '../components/assets/JustBlackjackLogo';
 import { gsap } from 'gsap';
-import { ScrollToPlugin } from "gsap/dist/ScrollToPlugin";
-
-gsap.registerPlugin(ScrollToPlugin);
 
 export default function Landing() {
+
+  // Create references to drive GSAP animations
+  const titleRef = useRef(null);
+  const roleRef = useRef([]);
+  const socialsRef = useRef(null);
 
   // Define list of roles to display
   const roleList = [
@@ -32,12 +34,6 @@ export default function Landing() {
       href: "https://www.mathkata.app/",
     },
   ]
-
-  // Create references to drive GSAP animations
-  const titleRef = useRef(null);
-  const roleRef = useRef([]);
-  const socialsRef = useRef(null);
-  const jBButtonRef = useRef(null);
 
   // Use shared context to tell role titles to be highlighted when 
   // social icons are highlighted
@@ -104,41 +100,6 @@ export default function Landing() {
       }
     );
 
-    // Initially disable pointer events on roles
-    gsap.set(jBButtonRef.current, { pointerEvents: "none" });
-
-    // Animate justBlackjack button
-    gsap.fromTo(
-      jBButtonRef.current, 
-      { opacity: 0 }, 
-      { 
-        opacity: 1, 
-        duration: 3, 
-        delay: 5, 
-        ease: "power1.inOut",
-        onComplete: function() {
-          gsap.set(jBButtonRef.current, { pointerEvents: "auto" })
-        } 
-      }
-    );
-
-    // Define animation functions for on-hover/focus for justBlackjack button
-    const onInteraction = (el) => {
-      gsap.to(el, { opacity: 0.7, duration: 0.2, ease: "sine.inOut" });
-    };
-
-    const afterInteraction = (el) => {
-      gsap.to(el, { opacity: 1, duration: 0.2, ease: "sine.inOut" });
-    };
-
-    // Assigning animations to the justBlackjack button
-    if (jBButtonRef.current) {
-
-      jBButtonRef.current.addEventListener("mouseover", () => onInteraction(jBButtonRef.current));
-      jBButtonRef.current.addEventListener("mouseout", () => afterInteraction(jBButtonRef.current));
-
-    }
-
   }, []);
 
   // Will run every time highlightedWord changes so that on-hover and 
@@ -195,9 +156,7 @@ export default function Landing() {
           </SocialHoverContext.Provider>
       </div>
       <DownArrow />
-      <div className="justblackjack-button" ref={jBButtonRef}>
-          <JustBlackjackLogo />
-        </div>
+      <JustBlackjackLogo />
     </div>
   )
 }
--- components/assets/JustBlackjackLogo.jsx
+++ components/assets/JustBlackjackLogo.jsx
@@ -1,24 +1,67 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState, useRef } from "react"
 import Lottie from "lottie-react";
 import justBlackjackAnim from "./justBlackjackLottie.json";
+import { gsap } from 'gsap';
 
 const JustBlackjackLogo = ({ ...props }) => {
 
     const [LogoSize, setLogoSize] = useState(0);
+    const jBButtonRef = useRef(null);
 
+    // Animate in
+    useEffect(() => {
+
+        // Initially disable pointer events on logo
+        gsap.set(jBButtonRef.current, { pointerEvents: "none" });
+
+        // Animate justBlackjack button
+        gsap.fromTo(
+            jBButtonRef.current, 
+            { opacity: 0 }, 
+            { 
+                opacity: 1, 
+                duration: 3, 
+                delay: 5, 
+                ease: "power1.inOut",
+                onComplete: function() {
+                gsap.set(jBButtonRef.current, { pointerEvents: "auto" })
+                } 
+            }
+        );
+
+        // Define animation functions for on-hover/focus for justBlackjack button
+        const onInteraction = (el) => {
+            gsap.to(el, { opacity: 0.7, duration: 0.2, ease: "sine.inOut" });
+        };
+
+        const afterInteraction = (el) => {
+            gsap.to(el, { opacity: 1, duration: 0.2, ease: "sine.inOut" });
+        };
+
+        // Assigning animations to the justBlackjack button
+        if (jBButtonRef.current) {
+
+            jBButtonRef.current.addEventListener("mouseover", () => onInteraction(jBButtonRef.current));
+            jBButtonRef.current.addEventListener("mouseout", () => afterInteraction(jBButtonRef.current));
+
+        }
+
+    }, []);
+
+    // Reactive sizing of logo
     useEffect(() => {
 
         const updateLogoSize = () => {
             const logoSize = window.innerWidth < 850 ? 100 : 150;
             setLogoSize(logoSize);
         }
-    
+
         // Call once to set initial measurements
         updateLogoSize();
-    
+
         // Listen to window resize event
         window.addEventListener('resize', updateLogoSize);
-    
+
         // Clean up event listener on unmount
         return () => {
             window.removeEventListener('resize', updateLogoSize);
@@ -31,11 +74,18 @@ const JustBlackjackLogo = ({ ...props }) => {
     // TODO: See if you can put the animation code here as well (on-hover, fade-in)
     // TODO: Can we do the same thing for the downarrow to declutter the landing component?
     // TODO: In the sidebar div, include a short spiel about what this is about- my firs tproj etc
-    return <Lottie 
-        animationData={justBlackjackAnim} 
-        style={{ height: `${LogoSize}px`, width: `${LogoSize}px` }} 
-        {...props}  
-    />;
+    return (
+        <div className="justblackjack-button" ref={jBButtonRef}>
+            <Lottie 
+                animationData={justBlackjackAnim} 
+                style={{ 
+                    height: `${LogoSize}px`, 
+                    width: `${LogoSize}px` 
+                }} 
+                {...props}  
+            />
+        </div>
+    );
 };
 
 export default JustBlackjackLogo;
