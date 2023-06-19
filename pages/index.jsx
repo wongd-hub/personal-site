3a367dd0ae1219bc50c5ca322e2ddd9c57dbc02b
Darren Wong <me@darrenwong.id.au>
2023-06-19 14:48:12 +1000

    Added Google Analytics script

--- pages/index.jsx
+++ pages/index.jsx
@@ -37,6 +37,22 @@ export default function Home() {
         <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/favicon-16x16.png" />
         <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+            page_path: window.location.pathname,
+          });
+        `,
+          }}
+        />
       </Head>
 
       <div className="grain-filter"/>
